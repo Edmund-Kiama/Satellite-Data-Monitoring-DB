cd01972c2cd2e9d377692ab7ea720553fa0a7f1b
Edmund <edmundgachanja@gmail.com>
2025-03-10 16:29:22 +0300

    feat:Adds display for all tables

--- main.py
+++ main.py
@@ -16,23 +16,103 @@
 #     - update the db
 
 from mydb.models import session, Satellite, SatelliteData, Region
-from sqlalchemy import select
+# from sqlalchemy import select
+from mydb.session import get_sats, get_reg, get_data, delete_sat_tb
 
 on_loop = True
 
+def sat_tb_display():
+    sats = get_sats()
+    print("")
+    print("                             Satellite Table")
+    print("-----------------------------------------------------------------------------------------")
+    print("| Id    |   Name        | Orbit Type |   Status   |   Description                       ")
+    print("-----------------------------------------------------------------------------------------")
+    for sat in sats:
+        print(f"| {sat.id:<3}   |   {sat.name:<10}  |   {sat.orbit_type:<5}    |   {sat.status:<7}  |   {sat.description:<20}")
+    print("-----------------------------------------------------------------------------------------")
+
+def satdata_tb_display():
+    data = get_data()
+    print("")
+    print("                             Satellite Data Table")
+    print("-------------------------------------------------------------------------------")
+    print("| Id    | Sat Id |       Data Type           |  Data Value   |  Date recorded ")
+    print("-------------------------------------------------------------------------------")
+    for dat in data:
+        print(f"| {dat.id:<3}   |   {dat.sat_id:<3}  |   {dat.data_type:<20}    |   {dat.data_value:<10}  |   {dat.date_recorded:<10}")
+    print("------------------------------------------------------------------------------")
+    
+def region_tb_display():
+    regions = get_reg()
+    print("")
+    print("                             Region Table")
+    print("------------------------------------------------------------------------")
+    print("| Id    | Sat Id |        Name               |  Latitude  |  Longitude ")
+    print("------------------------------------------------------------------------")
+    for reg in regions:
+        print(f"| {reg.id:<3}   |   {reg.sat_id:<3}  |   {reg.name:<20}    |   {reg.latitude:<7}  |   {reg.longitude:<7}")
+    print("------------------------------------------------------------------------")
+
+
 def display_table():
+    print("")
+    print("Choose Table to Display")
+    print("-----------------------")
+    print("1. Satellites table")
+    print("2. Satellites Data table")
+    print("3. Regions table")
+    print("")
+
+    while True:
+        user = input("Which table is to be displayed: ")
+        if user in ["1", "2", "3"]:
+            break
+        else:
+            print(f"{user} is Invalid Input. Choose in (1, 2, 3)")
+    
+    if user == "1":
+        sat_tb_display()
+    elif user == "2":
+        satdata_tb_display()
+    else:
+        region_tb_display()
+
+
     pass
 def create_table():
     pass
 def update_table():
     pass
 def delete_table():
-    pass
+    print("")
+    print("Choose Table to Delete")
+    print("-----------------------")
+    print("1. Satellites table")
+    print("2. Satellites Data table")
+    print("3. Regions table")
+    print("")
+
+    while True:
+        user = input("Which table is to be deleted: ")
+        if user in ["1", "2", "3"]:
+            break
+        else:
+            print(f"{user} is Invalid Input. Choose in (1, 2, 3)")
+    
+    if user == "1":
+        delete_sat_tb()
+    # elif user == "2":
+    #     satdata_tb_display()
+    # else:
+    #     region_tb_display()
+
 def exit_menu():
     global on_loop
     on_loop = False
 
 def menu():
+    print("")
     print("##################################")
     print("     SATELLITE MONITORING SYSTEM")
     print("")
@@ -51,7 +131,7 @@ def menu():
         if user in ["1", "2", "3", "4", "5"]:
             break;
         else:
-            print("f{user} is Invalid! Choose either 1, 2, 3, 4, 5.")
+            print("f{user} is Invalid! Choose in (1, 2, 3, 4, 5).")
 
     return user
 
