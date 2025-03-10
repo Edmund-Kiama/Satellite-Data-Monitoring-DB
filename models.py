847956a40faa5d204c6ac56fbd38cab8c8d0ffaf
Edmund <edmundgachanja@gmail.com>
2025-03-10 08:30:45 +0300

    feat: adds columns to model classes

--- models.py
+++ models.py
@@ -1,17 +1,51 @@
 from sqlalchemy.ext.declarative import declarative_base #type: ignore
 from sqlalchemy.orm import relationship, sessionmaker #type: ignore
-from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, create_engine #type: ignore
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, create_engine, Date #type: ignore
+from datetime import date
 
 Base = declarative_base()
 
 class Satellite(Base):
-    pass
+    
+    __tablename__ = 'satellites'
 
-class Satellite_data(Base):
-    pass
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    orbit_type = Column(String, nullable=False) # info: LEO,MEO,GEO
+    status = Column(String, nullable=False) # info: active, inactive
+
+    # note: relationship --> region & sat data
+    regions = relationship('Region', back_populates='satellite', cascade='all, delete-orphan')
+    satellite_data = relationship('SatelliteData', back_populates='satellite',cascade='all, delete-orphan')
+    
+
+class SatelliteData(Base):
+
+    __tablename__ = 'satellite_data'
+
+    id = Column(Integer, primary_key=True)
+    data_type = Column(String, )
+    data_value = Column(Integer)
+    date_recorded = Column(Date, default=date.today) # note: 2025-03-10
+
+    #note: relation --> sat id
+    sat_id = Column(Integer, ForeignKey('satellites.id'), nullable=False)
+    satellite = relationship('Satellite', back_populates='satellite_data')
+    
 
 class Region(Base):
-    pass
+
+    __tablename__ = 'regions'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
+
+    # note: relation --> sat id
+    sat_id = Column(Integer, ForeignKey('satellites.id'), nullable=False)
+    satellite = relationship('Satellite', back_populates='regions')
+    
 
 # note: Creates the engine
 engine = create_engine('sqlite:///monitoring.db')
