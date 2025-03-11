4012f3a303517c677575206008cde33406026ded
Edmund <edmundgachanja@gmail.com>
2025-03-11 19:05:21 +0300

    feat: adds display,delete and create ops to tkinter GUI

--- tk.py
+++ tk.py
@@ -114,17 +114,215 @@ def display_table():
 
 
 
-# MARK: CREATE TABLES
+# MARK: CREATE TABLES VARIABLES 
 
-# def create_table():
-#     messagebox.showinfo("Create", "Creating a new table value...")
+def handle_add_sat():
+    clear_screen()
+    
+    sats = get_sats()
+
+    tree = ttk.Treeview(root, columns=("Id", "Name", "Orbit Type", "Status", "Description"), show="headings")
+    
+    tree.heading("Id", text="Satellite Id")
+    tree.heading("Name", text="Satellite Name")
+    tree.heading("Orbit Type", text="Orbit Type")
+    tree.heading("Status", text="Status")
+    tree.heading("Description", text="Description")
+
+    for sat in sats:
+        tree.insert("", tk.END, values=(sat.id, sat.name, sat.orbit_type, sat.status, sat.description))
+
+    tree.pack(expand=True, fill="both", padx=10, pady=10)
+
+
+    form_label = tk.Label(root, text="Satellite Form", font=("Arial", 14))
+    form_label.pack(padx=10, pady=10)
+
+    name_label = tk.Label(root, text="Satellite Name:")
+    name_label.pack(padx=5, pady=5)
+    entry_name = tk.Entry(root)
+    entry_name.pack()
+
+    orbit_label = tk.Label(root, text="Orbit Type: ['MEO', 'LEO', 'GEO']")
+    orbit_label.pack(padx=5, pady=5)
+    entry_orbit = tk.Entry(root)
+    entry_orbit.pack()
+
+    status_label = tk.Label(root, text="Status: ['active', 'inactive']")
+    status_label.pack(padx=5, pady=5)
+    entry_status = tk.Entry(root)
+    entry_status.pack()
+
+    des_label = tk.Label(root, text="Description:")
+    des_label.pack(padx=5, pady=5)
+    entry_desc = tk.Entry(root)
+    entry_desc.pack()
+
+    def add_sat_details():
+        name = entry_name.get()
+        orbit_type = entry_orbit.get()
+        status = entry_status.get()
+        description = entry_desc.get()
+
+        if name and orbit_type and status and description:
+            add_sat(name, orbit_type, status, description)
+
+            messagebox.showinfo("Adding Info","New Satellite has been added.")
+            create_table_var()
+        else:
+            messagebox.showinfo("Info","Please fill the whole form")
+
+    add_btn = tk.Button(root, text="Add", command=add_sat_details)
+    add_btn.pack(padx=10, pady=10)
+
+    back_buttons(create_table_var)
+
+def handle_add_data():
+    clear_screen()
+    
+    data = get_data()
+
+    tree = ttk.Treeview(root, columns=("Id", "Sat Id", "Data Type", "Data Value", "Date Recorded"), show="headings")
+    
+    tree.heading("Id", text="Id")
+    tree.heading("Sat Id", text="Satellite Id")
+    tree.heading("Data Type", text="Data Type")
+    tree.heading("Data Value", text="Data Value")
+    tree.heading("Date Recorded", text="Date Recorded")
+
+    for dat in data:
+        tree.insert("", tk.END, values=(dat.id, dat.sat_id, dat.data_type, dat.data_value, dat.date_recorded))
+
+    tree.pack(expand=True, fill="both", padx=10, pady=10)
+
+
+    form_label = tk.Label(root, text="Satellite Data Form", font=("Arial", 14))
+    form_label.pack(padx=10, pady=10)
+
+    sat_ids = get_sat_ids()
+    sat_id_label = tk.Label(root, text=f"Satellite Id {sat_ids}:")
+    sat_id_label.pack(padx=5, pady=5)
+    entry_sat_id = tk.Entry(root)
+    entry_sat_id.pack()
+
+    type_label = tk.Label(root, text="Data Type: ")
+    type_label.pack(padx=5, pady=5)
+    entry_type = tk.Entry(root)
+    entry_type.pack()
+
+    value_label = tk.Label(root, text="Data Value: ")
+    value_label.pack(padx=5, pady=5)
+    entry_value = tk.Entry(root)
+    entry_value.pack()
+
+    date_label = tk.Label(root, text="Date Recorded: ")
+    date_label.pack(padx=5, pady=5)
+    entry_date = tk.Entry(root)
+    entry_date.pack()
+
+    def add_data_details():
+        sat_id = entry_sat_id.get()
+        data_type = entry_type.get()
+        data_value = entry_value.get()
+        date_recorded = entry_date.get()
+
+        if sat_id and data_type and data_value and date_recorded:
+            add_data(sat_id, data_type, data_value, date_recorded)
+
+            messagebox.showinfo("Adding Info","New Satellite Data has been added.")
+            create_table_var()
+        else:
+            messagebox.showinfo("Info","Please fill the whole form")
+
+    add_btn = tk.Button(root, text="Add", command=add_data_details)
+    add_btn.pack(padx=10, pady=10)
+
+    back_buttons(create_table_var)
+
+def handle_add_reg():
+    clear_screen()
+    
+    regions = get_reg()
+
+    tree = ttk.Treeview(root, columns=("Id", "Sat Id", "Name", "Latitude", "Longitude"), show="headings")
+    
+    tree.heading("Id", text="Id")
+    tree.heading("Sat Id", text="Satellite Id")
+    tree.heading("Name", text="Name")
+    tree.heading("Latitude", text="Latitude")
+    tree.heading("Longitude", text="Longitude")
+
+    for reg in regions:
+        tree.insert("", tk.END, values=(reg.id, reg.sat_id, reg.name, reg.latitude, reg.longitude))
+
+    tree.pack(expand=True, fill="both", padx=20, pady=20)
+
+
+    form_label = tk.Label(root, text="Region Form", font=("Arial", 14))
+    form_label.pack(padx=10, pady=10)
+
+    sat_ids = get_sat_ids()
+    sat_id_label = tk.Label(root, text=f"Satellite Id {sat_ids}:")
+    sat_id_label.pack(padx=5, pady=5)
+    entry_sat_id = tk.Entry(root)
+    entry_sat_id.pack()
+
+    name_label = tk.Label(root, text="Region Name: ")
+    name_label.pack(padx=5, pady=5)
+    entry_name = tk.Entry(root)
+    entry_name.pack()
+
+    lat_label = tk.Label(root, text="Latitude: ")
+    lat_label.pack(padx=5, pady=5)
+    entry_lat = tk.Entry(root)
+    entry_lat.pack()
+
+    long_label = tk.Label(root, text="Longitude: ")
+    long_label.pack(padx=5, pady=5)
+    entry_long = tk.Entry(root)
+    entry_long.pack()
+
+    def add_reg_details():
+        sat_id = entry_sat_id.get()
+        name = entry_name.get()
+        latitude = entry_lat.get()
+        longitude = entry_long.get()
+
+        if sat_id and name and latitude and longitude:
+            add_data(sat_id, name, latitude, longitude)
+
+            messagebox.showinfo("Adding Info","New Region has been added.")
+            create_table_var()
+        else:
+            messagebox.showinfo("Info","Please fill the whole form")
+
+    add_btn = tk.Button(root, text="Add", command=add_reg_details)
+    add_btn.pack(padx=10, pady=10)
+
+
+def create_table_var():
+    clear_screen()
+
+    label = tk.Label(root, text="Choose Table to add new instance", font=("Arial", 14, "bold"))
+    label.pack(padx=10, pady=10)
+
+    btn1 = tk.Button(root, text="Satellite Table", command=handle_add_sat)
+    btn1.pack(pady=5)
+
+    btn2 = tk.Button(root, text="Satellite Data Table", command=handle_add_data)
+    btn2.pack(pady=5)
+
+    btn3 = tk.Button(root, text="Region Table", command=handle_add_reg)
+    btn3.pack(pady=5)
+
+    back_buttons()
 
 
 
 # MARK: UPDATE TABLES
 
-# def update_table():
-#     messagebox.showinfo("Update", "Updating a table value...")
+def update_table():
+    messagebox.showinfo("Update", "Updating a table value...")
 
 
 
@@ -275,11 +473,11 @@ def main_menu():
     btn1 = tk.Button(root, text="Display Tables", command=display_table)
     btn1.pack(pady=5)
 
-    # btn2 = tk.Button(root, text="Create New Table Value", command=create_table)
-    # btn2.pack(pady=5)
+    btn2 = tk.Button(root, text="Create New Table Value", command=create_table_var)
+    btn2.pack(pady=5)
 
-    # btn3 = tk.Button(root, text="Update Table Value", command=update_table)
-    # btn3.pack(pady=5)
+    btn3 = tk.Button(root, text="Update Table Value", command=update_table)
+    btn3.pack(pady=5)
 
     btn4 = tk.Button(root, text="Delete Table Value", command=delete_table)
     btn4.pack(pady=5)
